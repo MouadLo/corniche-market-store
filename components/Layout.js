e82988139c5b5b6ac8082a274ec0eb47e9e304bd
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-10-15 04:20:27 +0100

    add styles to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 
+import useStyles from '../utils/styles';
+
 export default function Layout({ children }) {
+	const classes = useStyles();
 	return (
 		<div>
 			<Head>
 				<title>Corniche Market</title>
 			</Head>
-			<AppBar position="static">
+			<AppBar position="static" className={classes.navbar}>
 				<Toolbar>
 					<Typography>Corniche Market</Typography>
 				</Toolbar>
 			</AppBar>
-			<Container>{children}</Container>
-			<footer>
+			<Container className={classes.main}>{children}</Container>
+			<footer className={classes.footer}>
 				<Typography>All right reserved Corniche Market</Typography>
 			</footer>
 		</div>
